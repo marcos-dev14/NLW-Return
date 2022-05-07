e8bc95a2696c9f66086f5d126689d28c782d4bc0
marcospaulodev <suporte@tgoo.pt>
2022-05-07 12:12:08 -0300

    Ajuste do back-end para o deploy

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, // Foi adicionado a uma variável de ambiente para não enviar a rota
 });
